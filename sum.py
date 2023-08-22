60fd8df6862648f884815c87f4a367a0678e1d42
Vidhi Patel <vidhipatel6897@gmail.com>
2023-08-22 17:35:02 +0530

    initial commit

---
+++ sum.py
@@ -0,0 +1,13 @@
+def add(a,b):
+  return a+b
+ 
+#initializing the variables
+num1 = 10
+num2 = 5
+ 
+#function calling and store the result into sum_of_twonumbers
+sum_of_twonumbers = add(num1,num2)
+ 
+#To print the result
+print("Sum of {0} and {1} is {2};" .format(num1,
+                           num2, sum_of_twonumbers))
